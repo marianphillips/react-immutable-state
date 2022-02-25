9918cbfaeab6a38edbb5a4461130e14715a6142f
marianphillips <79763160+marianphillips@users.noreply.github.com>
2022-02-25 17:16:07 +0000

    completed with extension

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import "./App.css"
 
 function App() {
   const [workouts, setWorkouts] = useState(initialWorkouts)
+  const [showCompletedOnly, setShowCompletedOnly] = useState(false)
 
   const addNewWorkout = () => {
     const newWorkout = generateWorkout()
@@ -20,23 +21,36 @@ function App() {
     setWorkouts(updatedWorkouts)
   }
 
+  const replaceWorkout = (workout) => {
+    const newWorkout = generateWorkout()
+    const updatedWorkouts = workouts.map(wko => wko === workout ? newWorkout : wko)
+    setWorkouts(updatedWorkouts)
+  }
+
+  const liElement = (workout, index) => {
+    return <li key={index}>
+    <p>
+      {workout.sets}x sets of <strong>{workout.reps}x{workout.exercise}</strong> with {workout.rest} seconds rest
+    </p>
+    {!workout.done && 
+      <button onClick={e=>completeWorkout(workout)}>Done</button>}
+    {workout.done && 
+     <p>✅</p>}
+    <button onClick={e=>deleteWorkout(workout)}>Delete</button>
+    <button onClick={e=>replaceWorkout(workout)}>Replace</button>
+  </li>
+  }
+
   return (
     <div className="App">
       <h1>🏋️‍♀️Workout Generator</h1>
       <button onClick={addNewWorkout}>Add New Workout</button>
+      <br/><br/>
+      <label>Show Completed Only</label>
+      <input type="checkbox" defaultChecked={showCompletedOnly} onChange={() => setShowCompletedOnly(!showCompletedOnly)}/>
       <ul>
-        {workouts.map((workout, index) => (
-          <li key={index}>
-            <p>
-              {workout.sets}x sets of <strong>{workout.reps}x{workout.exercise}</strong> with {workout.rest} seconds rest
-            </p>
-            {!workout.done && 
-              <button onClick={e=>completeWorkout(workout)}>Done</button>}
-            {workout.done && 
-             <p>✅</p>}
-            <button onClick={e=>deleteWorkout(workout)}>Delete</button>
-          </li>
-        ))}
+        {!showCompletedOnly && workouts.map((workout, index) => liElement(workout, index))}
+        {showCompletedOnly && workouts.filter(workout => workout.done).map((workout, index) => liElement(workout, index))}
       </ul>
       
     </div>
